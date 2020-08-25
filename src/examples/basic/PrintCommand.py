b851d6fd3dec67a7ed2cf1b808ac74646061d8ba
dshapiro1dev <69366600+dshapiro1dev@users.noreply.github.com>
2020-08-25 08:57:54 -0400

    more

--- src/examples/basic/PrintCommand.py
+++ src/examples/basic/PrintCommand.py
@@ -3,3 +3,8 @@
 
 # print a basic string
 print("This is a basic print")
+
+# you need parentheses - the print statement below will not work - uncomment and try it
+# notice that if you leave this line in - Python will tell you
+# "SyntaxError: Missing parentheses in call to 'print'. Did you mean print("This won't work , you need parentheses")?
+# print "This won't work , you need parentheses"
