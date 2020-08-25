d488deb465ec8f66f43b1af948975952cb62abef
dshapiro1dev <69366600+dshapiro1dev@users.noreply.github.com>
2020-08-25 10:24:07 -0400

    bit of comments

--- src/examples/basic/HelloWorld.py
+++ src/examples/basic/HelloWorld.py
@@ -1,1 +1,8 @@
+#   The # is a comment - it is put at a front of a line , to inform the reader only
+#   Python does not read any lines starting with a # - it jumps right over them. They are called comments
+
+# this is the very first program any writes - ever
+# it prints "hello world" to the screen
+
+# Welcome to python
 print("hello world")
