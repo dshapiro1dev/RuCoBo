a891cbb15e30ae84d3d7de9e6e79f843691507a5
dshapiro1dev <69366600+dshapiro1dev@users.noreply.github.com>
2020-08-29 20:57:50 -0400

    working with files

---
+++ src/examples/basic/WorkingWithFiles.py
@@ -0,0 +1,5 @@
+# Files: documents containing typically text or data
+# files live in directories on our disk, and remain after we reboot computer
+
+# Python can read in files to use them or can write out files to disk
+
