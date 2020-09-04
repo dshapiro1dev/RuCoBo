466e0ecb1551fc2f2deeff1ba133ce886ed9748c
dshapiro1dev <69366600+dshapiro1dev@users.noreply.github.com>
2020-09-03 21:48:05 -0400

    yak tartar

--- src/examples/builtin_types/Dictionaries.py
+++ src/examples/builtin_types/Dictionaries.py
@@ -53,3 +53,59 @@ print(meats)
 # and then we can cycle through each delicious critter to see what cooking temperatures are, like
 for critter in meats:  # go through each critter
     print("If you're grilling :" + str(critter) + " you should cook to a temperature  : " + str(temperatures[critter]))
+
+# you can not have 2 keys in a dictionary which are the same
+lovesmost = {
+    'dan' : 'steak',
+    'dan' : 'yak burger with fries and a side of mayo on a sesame roll with ketchup'
+}
+print(lovesmost)
+# here there is only one key: 'dan'  - it _must_ be unique
+# so python takes the 2nd entry as the only entry  [ clearly thats the superior entry anyway ]
+# ie: a dictionary can not have 2 different values for the same key
+
+# now each key:value pair is an item in the dictionary
+# technically it is a tuple (we'll learn about those soon)
+# so lets iterate through all the items
+alltuples = temperatures.items()
+for t in alltuples:
+    print("key : " + t[0] + "  -> stores value: " + str(t[1]))
+# note here:  for the tuple  key:value
+# to access the first part (ie: the key) we take the 0th index  t[0]
+# to access the second part (ie: the value) we take the 1th index t[1]
+
+# another way to cycle through a dictionary is over the list of keys
+keylist = temperatures.keys()
+for k in keylist:
+    print(" the key is: " + k + " and the value : " + str(temperatures[k]))
+# notice again that we always convert to a string, using str()
+
+# now lets get clever - and make a dictionary of dictionaries
+yak_recipes = {
+    'yak stroganoff' : 'one yak, one strog, lot of noff, 2 onions',
+    'yak pasta' : 'one yak, one enormous bowl of pasta',
+    'yak tartar' : 'one yak, one very large blender'
+}
+iguana_recipes = {
+    'iguana shake' : 'one iguana, a teaspoon of salt, morphine',
+    'iguana burger' : 'one iguana, one bun, 3 tubs of ketchup'
+}
+# first we created 2 dictionaries
+# yak_recipes:  has a few yak recipes
+# igauana_recipes: has a few iguana recipes
+
+# and now our dictionary of dictionaries
+recipe_book = {
+        'yak dict' : yak_recipes,
+        'iguana_dict' : iguana_recipes
+}
+
+# lets print out the recipe book
+print(recipe_book)
+
+# and if you want to access an entry - the doubly nested reference
+print(recipe_book['yak dict']['yak tartar'])
+# this selects first the yak dictionary, and within that the 'yak tartar' recipe
+
+# you can nest arbitrary number of dictionaries (or any other structure) within other structures ...
+
