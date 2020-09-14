aa8e291218d021d3bb9db30d3f3be8fb7703f38e
dshapiro1dev <69366600+dshapiro1dev@users.noreply.github.com>
2020-09-14 15:51:21 -0400

    Update recursion.py

--- src/examples/functional/recursion.py
+++ src/examples/functional/recursion.py
@@ -1,1 +1,5 @@
-# recursion
+# recursion is when a function calls itself
+# this needs to be done with care so that you don't create an infinite loop
+# but recursion can solve some complex problems in a simple way - and its fun to play with
+
+
