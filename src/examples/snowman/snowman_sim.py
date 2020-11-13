614c3fb273d26b0fc8c53726c9c7f81dd0de5128
Brian Bozzuto <brian@brianbozzuto.com>
2020-11-12 22:52:39 -0500

    Added two strategies "frequency", will pick letters in abstract descending order based on a generic frequency of letters in the english language
    "freqency by length" will look at all the available words and weight each letter based on words of a specified length

--- src/examples/snowman/snowman_sim.py
+++ src/examples/snowman/snowman_sim.py
@@ -4,15 +4,35 @@ import snowman_solver
 
 print("Starting a game")
 
-challenge_word = word_selector.get_word()
-my_solver = snowman_solver.Solver(len(challenge_word))
-my_game = snowman.Game(challenge_word, 5)
-tries = 0
 
-while my_game.word_revealed is False:
-    tries += 1
-    guess = my_solver.make_guess()
-    result = my_game.guess(guess)
-    print(f"Word so far: {my_game.revealed_word()} Guessing the letter {guess}: {result}")
 
-print(f"Found the work {my_game.word} in {tries} tries")
+round = 0
+games = 100
+wins = 0
+guesses_needed = []
+
+while round < games:
+    round += 1
+    tries = 0
+    game_result = "Loss"
+    challenge_word = word_selector.get_word()
+    my_solver = snowman_solver.Solver("frequency by length", len(challenge_word))
+    my_game = snowman.Game(challenge_word, 12)
+    while my_game.word_revealed is False:
+        tries += 1
+        guess = my_solver.make_guess()
+        round_result = my_game.guess(guess)
+        # print(f"Word so far: {my_game.revealed_word()} Guessing the letter {guess}: {result}")
+    if tries <= my_game.tries:
+        wins += 1
+        game_result = "Win"
+    guesses_needed.append(tries)
+    print(f"Round {round}: Word is {challenge_word}, result: {game_result}")
+
+
+
+print(f"Played {games} rounds and won {wins}")
+print(f"Win rate: {wins/games}")
+print(f"Average guesses to solve: {sum(guesses_needed)/len(guesses_needed)}")
+print(f"Least guesses to solve: {min(guesses_needed)}")
+print(f"Most guesses to solve: {max(guesses_needed)}")
--- src/examples/snowman/snowman_solver.py
+++ src/examples/snowman/snowman_solver.py
@@ -4,21 +4,57 @@ import random
 class Solver:
     """This is where we come up with cool strategies for solving snowman puzzles"""
 
-    def __init__(self, word_length):
+    def __init__(self, strategy="random", word_length=10):
         self.word_length = word_length
-        self.possible_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
-                                 'r', 's', 't',
-                                 'u', 'v', 'w', 'x', 'y', 'z']
+        self.possible_letters = get_possible_letters()
+        self.frequent_letters = list("etaoinsrhldcumfpgwybvkxjqz")
+        self.strategy = strategy
+        self.guess_list = []
 
     def make_guess(self):
-        index = random.randint(0, len(self.possible_letters) - 1)
-        guess = self.possible_letters.pop(index)
+        if self.strategy == "frequency":
+            guess = self.frequent_letters.pop(0)
+        elif self.strategy == "frequency by length":
+            if len(self.guess_list) == 0:
+                self.guess_list = get_frequency(self.word_length)
+            guess = self.guess_list.pop(0)
+        else:
+            index = random.randint(0, len(self.possible_letters) - 1)
+            guess = self.possible_letters.pop(index)
+
         return guess
 
-print("testing")
-testgame = Solver(10)
 
-x = 0
-while x < 10:
-    print(testgame.make_guess())
-    x +=1
+def get_possible_letters():
+    return list("abcdefghijklmnopqrstuvwxyz")
+
+
+def get_frequency(length=0):
+    dict = get_dictionary(length)
+    freq = {i: 0 for i in get_possible_letters()}
+    for word in dict:
+        for l in word:
+            freq[l] += 1
+    return sorted(freq, key= lambda x: freq[x], reverse=True)
+
+
+def get_dictionary(length=0):
+    whitelist = set('abcdefghijklmnopqrstuvwxyz\n')  # only use these characters - remove everything else
+    fname = "/usr/share/dict/words"
+    fhand = open(fname)
+    data = fhand.read().lower()
+    fdata = ''.join(filter(whitelist.__contains__, data))
+    words = fdata.split('\n')
+    dict = []
+    for w in words:
+        if length > 0:
+            if len(w) == length:
+                dict.append(w)
+        else:
+            if len(w) > 1:
+                dict.append(w)
+    return dict
+
+
+# print("testing")
+# print(get_frequency(6))
