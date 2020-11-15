8f12632dbdf96600083e73767c1e78e18d746bed
dshapiro1dev <69366600+dshapiro1dev@users.noreply.github.com>
2020-11-15 07:13:28 -0500

    added adaptive algorithm to snowman: ds_adaptive

--- src/examples/snowman/snowman_sim.py
+++ src/examples/snowman/snowman_sim.py
@@ -7,27 +7,52 @@ print("Starting a game")
 
 
 round = 0
-games = 100
+games = 50000
 wins = 0
 guesses_needed = []
+
+stats = {} # track statistics
+
 
 while round < games:
     round += 1
     tries = 0
     game_result = "Loss"
     challenge_word = word_selector.get_word()
-    my_solver = snowman_solver.Solver("frequency by length", len(challenge_word))
+#    my_solver = snowman_solver.Solver("frequency by length", len(challenge_word))
+    my_solver = snowman_solver.Solver("ds adaptive", len(challenge_word))  # 93 % success
+
     my_game = snowman.Game(challenge_word, 12)
+
+    # keep statistics
+    length = len(my_game.word) # length of the word
+    if length in stats:
+        stats[length]['num'] += 1
+    else:
+        stats[length] = {}
+        stats[length]['num'] = 1
+        stats[length]['win'] = 0
+
     while my_game.word_revealed is False:
         tries += 1
-        guess = my_solver.make_guess()
+        guess = my_solver.make_guess(my_game.revealed_letters)
         round_result = my_game.guess(guess)
         # print(f"Word so far: {my_game.revealed_word()} Guessing the letter {guess}: {result}")
     if tries <= my_game.tries:
         wins += 1
+        print(f"Win rate: {wins / round}")
         game_result = "Win"
+        stats[length]['win'] += 1
+
+    # output stats
+#    print(stats)
+    print('STATISTICS SO FAR')
+    for ll in sorted(stats.keys()):
+        pct = stats[ll]['win'] / stats[ll]['num'] * 100
+        print(f" Round: {round}  Length: {ll}  Num: {stats[ll]['num']}  Wins: {stats[ll]['win']}   Pct: {pct}")
+
     guesses_needed.append(tries)
-    print(f"Round {round}: Word is {challenge_word}, result: {game_result}")
+ #   print(f"Round {round}: Word is {challenge_word}, result: {game_result}")
 
 
 
--- src/examples/snowman/snowman_solver.py
+++ src/examples/snowman/snowman_solver.py
@@ -10,23 +10,83 @@ class Solver:
         self.frequent_letters = list("etaoinsrhldcumfpgwybvkxjqz")
         self.strategy = strategy
         self.guess_list = []
+        self.guessed_so_far = []
 
-    def make_guess(self):
+    def make_guess(self,revealed_so_far):
         if self.strategy == "frequency":
             guess = self.frequent_letters.pop(0)
         elif self.strategy == "frequency by length":
             if len(self.guess_list) == 0:
                 self.guess_list = get_frequency(self.word_length)
             guess = self.guess_list.pop(0)
+        elif self.strategy == "ds adaptive":
+            guess = self.ds_adaptive(revealed_so_far)
         else:
             index = random.randint(0, len(self.possible_letters) - 1)
             guess = self.possible_letters.pop(index)
 
         return guess
 
+    # TO DO:
+    # 1. if  'e' appears in location 'i' only - remove all words w/ 'e' in non-i location
+    # 2. remove words that dont have 'e'  at all - if 'e' is revealed as in word
+    # 3. choose a letter (all else being equal) - that will cut the tree the most on next round
+    def ds_adaptive(self,reveal):
+        # get dictionary of words only this long
+        dict = get_dictionary(len(reveal))
+
+        # helper variables
+        length = len(reveal) # length of word
+        removes = {}         # dictionary of words to remove - that are mismatched
+        revletters = {}      # dictionary of revealed letters
+        for l in reveal:
+            if l!='_':
+                revletters[l]=1
+
+        # subselect a dictionary with only words that match revealed pattern so far
+        for lword in dict:
+            llist = list(lword)  # convert word into list of letters
+            # filter  : known letter at known position - not there in the word in dictionary
+            #           known letter in dictionary word at a position - but is not in the "reveal" at that position
+            for i in range(0,length):
+                if reveal[i]!='_' and llist[i]!=reveal[i]:
+                    removes[lword] = 1
+                if llist[i] in revletters.keys() and reveal[i]=='_':
+                    removes[lword] = 1
+
+        # create cleaned dictionary only with matching words
+        cleandict = []
+        for lword in dict:
+            if lword not in removes:
+                cleandict.append(lword)
+
+        # get frequency only for a specified dictionary
+        adaptive_list = get_frequency_for_dict(cleandict)
+
+        # make best guess
+        found_guess = False
+        k = 0
+        guess = 'a'
+        while not found_guess:
+            if adaptive_list[k] not in self.guessed_so_far:
+                guess = adaptive_list[k]
+                found_guess = True
+            k += 1
+
+        # record the guess
+        self.guessed_so_far.append(guess)
+
+        return guess
 
 def get_possible_letters():
     return list("abcdefghijklmnopqrstuvwxyz")
+
+def get_frequency_for_dict(dict):
+    freq = {i: 0 for i in get_possible_letters()}
+    for word in dict:
+        for l in word:
+            freq[l] += 1
+    return sorted(freq, key= lambda x: freq[x], reverse=True)
 
 
 def get_frequency(length=0):
