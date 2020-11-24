c63de0f029a706abca58a010cfbbb2778b4af7a4
dshapiro1dev <69366600+dshapiro1dev@users.noreply.github.com>
2020-11-23 20:43:31 -0500

    final few changes

--- src/examples/snowman/run_contest.py
+++ src/examples/snowman/run_contest.py
@@ -26,6 +26,7 @@ for r in range(0,num_games):
     # pick a word - create a snowman game for it
     word = word_selector.get_word()
     game = snowman.Game(word, num_allowed)
+    print("Running  word is: ",word," <- ")
 
     # initialize guesser
     guesser.initialize(len(word))
