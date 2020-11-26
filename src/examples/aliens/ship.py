972590e5233f07b0ee50e7ff54de7fb6b79950c0
Brian Bozzuto <brian@brianbozzuto.com>
2020-11-25 22:10:39 -0500

    Finished getting the ship to move and shoot. Using this as a base model is going to be tricky, because if you go through the book they refactor the code. It's nice how they do it, but may make individual steps harder to see here.

--- src/examples/aliens/ship.py
+++ src/examples/aliens/ship.py
@@ -12,8 +12,12 @@ class Ship:
         # Load the ship impage and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
+
         # start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position
+        self.x = float(self.rect.x)
 
         # Movement flag
         self.moving_right = False
@@ -21,12 +25,13 @@ class Ship:
 
     def update(self):
         """update the ships location base on the movement flag"""
-        if self.moving_right:
-            if self.rect.x < (self.settings.screen_width - self.rect.width):
-                self.rect.x += 1
-        if self.moving_left:
-            if self.rect.x > 0:
-                self.rect.x -= 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.x > 0:
+            self.x -= self.settings.ship_speed
+
+        # update rect object from self.x
+        self.rect.x = self.x
 
     def blitme(self):
         """draw the ship at its current location"""
