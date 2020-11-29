83cb11539b046ce6d75d3a796bff76d87d12d152
Brian Bozzuto <brian@brianbozzuto.com>
2020-11-28 21:45:43 -0500

    Code now updated through Chapter 12

--- src/games/aliens/bullet.py
+++ src/games/aliens/bullet.py
@@ -5,22 +5,23 @@ from pygame.sprite import Sprite
 class Bullet(Sprite):
     """A class to manage bullets fired from the ship."""
 
-    def __init__(self, ai_settings, screen, ship):
+    def __init__(self, ai_game):
         """Create a bullet object at the ship's current position."""
-        super(Bullet, self).__init__()
-        # super().__init__()
-        self.screen = screen
+        super().__init__()
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.color = self.settings.bullet_color
 
         # Create a bullet rect at (0, 0) and then set correct position.
-        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width, ai_settings.bullet_height)
-        self.rect.centerx = ship.rect.centerx
-        self.rect.top = ship.rect.top
+        self.rect = pygame.Rect(0, 0, self.settings.bullet_width, self.settings.bullet_height)
+        self.rect.midtop = ai_game.ship.rect.midtop
+
 
         # Store the bullet's position as a decimal value
         self.y = float(self.rect.y)
 
-        self.color = ai_settings.bullet_color
-        self.speed_factor = ai_settings.bullet_speed_factor
+        self.color = self.settings.bullet_color
+        self.speed_factor = self.settings.bullet_speed
 
     def update(self):
         """Move the bullet up the screen."""
