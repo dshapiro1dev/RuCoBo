ce90cc42c9032e5f67950659bd231bddb5cd04f1
dshapiro1dev <69366600+dshapiro1dev@users.noreply.github.com>
2020-12-15 08:29:12 -0500

    working with lists

---
+++ src/exercises/ManipulateLists.py
@@ -0,0 +1,45 @@
+# This puzzle works you through various functionalities of lists
+# If you do all the steps correctly - you will be able to fix the google doc address and get the prize
+
+
+# fix this google doc address - and find the prize
+mangled = "https://docs.google.com/document/d/1FK7pUWYt139erPO48fIc0ukHkpSYUhkAwYWPwTMfn8owmxlR7OnkYgo/edit?usp=sharing"
+
+# make an empty list called:  list1
+
+# put the numbers 12 through 255 inclusive into list1  [ inclusive means  12,13,...,255  , including the 12 and 255 ]
+
+# select every 13th entry of list1 , starting at and including the 11th entry - put the results into list2
+
+# remove the last 8 entries of list2
+
+# remove the first 2 entries of list2
+
+# convert the string "YakAttacksAreANormalPartOfLife"   into a list of letters - call it list3
+
+# change list3 to only keep every 4th entry from its original list3
+
+# remove the first letter 'k'  from list3
+
+# make a copy of list2 , call it list4
+
+# set the 5th element of list4 to be 37
+
+# create list5 which is  the elements of list2 followed by elements of list4 , in a single list
+
+# append 19 to list5
+
+# keep only each 7th entry in list5
+
+# insert the 2nd element of list5  into location 2  of list3
+
+# insert the 1st element of list5 into location 7 of list3
+
+# now lets print out the characters in list3
+for i in list3:
+    print(i,end="")
+print()
+
+# the string you just printed  will have some letters and numbers
+# look for that string the mangled address above - and delete it.
+# what is left will be an address for a google doc - go there and leave your mark
