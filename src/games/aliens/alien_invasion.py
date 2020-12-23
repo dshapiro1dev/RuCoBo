620dc0e2fc04b5b77f70de1f122a9a760b196ab4
Brian Bozzuto <brian@brianbozzuto.com>
2020-12-23 11:51:12 -0500

    Completed chapter 13

--- src/games/aliens/alien_invasion.py
+++ src/games/aliens/alien_invasion.py
@@ -36,9 +36,12 @@ class AlienInvasion:
         while True:
             # Watch for keyboard and mouse events
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _change_fleet_direction(self):
@@ -139,19 +142,23 @@ class AlienInvasion:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
 
-        # Decrement ships_left.
-        self.stats.ships_left -= 1
+        # Confirm the player still has ships, otherwise the game over
+        if self.stats.ships_left > 0:
+            # Decrement ships_left.
+            self.stats.ships_left -= 1
 
-        # Get rid of any remaining aliens and bullets
-        self.aliens.empty()
-        self.bullets.empty()
+            # Get rid of any remaining aliens and bullets
+            self.aliens.empty()
+            self.bullets.empty()
 
-        #Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
+            #Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
 
-        #pause
-        sleep(0.5)
+            #pause
+            sleep(0.5)
+        else:
+            self.stats.game_active = False
 
     def _update_aliens(self):
         """Update the position of all aliens in the fleet."""
--- src/games/aliens/game_stats.py
+++ src/games/aliens/game_stats.py
@@ -6,6 +6,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # Start Alien invasion in an inactive state
+        self.game_active = False
+
     def reset_stats(self):
         """Initialize statistics that can change during the game"""
         self.ships_left = self.settings.ship_limit
