e789d61c5c8be847587905f478feb64a61e0370a
Brian Bozzuto <brian@brianbozzuto.com>
2021-03-01 21:51:21 -0500

    Updated the script to keep scrolling every 2 seconds until the page fully loads so that it can get the complete list of ski mountains

--- projects/trevor/ski_mountain_details.py
+++ projects/trevor/ski_mountain_details.py
@@ -6,16 +6,24 @@ driver = Chrome()
 resort_info = {}
 
 # try to go through one mountain and collect information
-driver.get("https://www.onthesnow.com/north-carolina/sugar-mountain-resort/ski-resort.html")
+driver.get("https://www.onthesnow.com/maine/sugarloaf/ski-resort.html")
 time.sleep(3)
 
 # Get the resort Name
 resort_name = driver.find_element_by_xpath("//span[contains(@class, 'resort_name')]").text
 resort_info["name"] = resort_name
 
+# Get the longest run
+longest_run = driver.find_element_by_xpath("//ul[contains(@class, 'rt_trail diamonds')]/li[8]/p[2]").text
+resort_info["longest_run"] = longest_run
+
 # Get the Max elevation
 resort_summit = driver.find_element_by_xpath("//li[contains(@class, 'top')]/div[1]").text
 resort_info["summit"] = resort_summit
+
+# Get the Drop in elevation
+resort_drop = driver.find_element_by_xpath("//li[contains(@class, 'drop')]/div[1]").text
+resort_info["drop"] = resort_drop
 
 # Get the lift info
 resort_trams = driver.find_element_by_xpath("//li[contains(@class, 'trams')]").text
--- projects/trevor/ski_mountain_list.py
+++ projects/trevor/ski_mountain_list.py
@@ -1,26 +1,48 @@
 from selenium.webdriver import Chrome
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
+
 Chrome(executable_path="/opt/WebDriver/bin/chromedriver")
 driver = Chrome()
 
+driver.get("https://www.onthesnow.com/united-states/skireport.html")
+"""x = 0
+while x < 20:
+    time.sleep(2)
+    driver.execute_script("window.scrollTo(0,document.body.scrollHeight)")
+    x += 1"""
+
+reached_page_end = False
+last_height = driver.execute_script("return document.body.scrollHeight")
 
 
-driver.get("https://www.onthesnow.com/new-england/skireport.html")
-time.sleep(5)
-elements = driver.find_elements_by_tag_name("a")
+while not reached_page_end:
+    driver.find_element_by_xpath('//body').send_keys(Keys.END)
+    time.sleep(2)
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if last_height == new_height:
+        reached_page_end = True
+    else:
+        last_height = new_height
+
+# elements = driver.find_elements_by_tag_name("a")
+# elements = driver.find_elements_by_xpath("//a[contains(@style, 'font-size: 11px;')]")
+elements =WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.XPATH, "//a[contains(@style, 'font-size: 11px;')]")))
 
 resortlinks = []
 resort = {}
 
+
+
 for item in elements:
     link = item.get_attribute("href")
     title = item.get_attribute("title")
-    try:
-        if "/ski-resort.html" in link:
-            print(f"found {title} at {link}")
-            resortlinks.append(link)
-    except (TypeError):
-        continue
+        #if "/ski-resort.html" in link:
+    if link.count("/") == 5:
+        print(f"found {title} at {link}")
+        resortlinks.append(link)
 
-
-# driver.quit()
+driver.quit()
