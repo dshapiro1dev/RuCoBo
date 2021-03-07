91844365b8750e25b837a28c3e3aab952ca32488
Brian Bozzuto <brian@brianbozzuto.com>
2021-03-06 19:03:10 -0500

    Expanded the data set of mountain information retrieved

--- projects/trevor/ski_mountain_details.py
+++ projects/trevor/ski_mountain_details.py
@@ -1,5 +1,7 @@
 from selenium.webdriver import Chrome
 import time
+
+
 # Chrome(executable_path="/opt/WebDriver/bin/chromedriver")
 
 class ski_mountain():
@@ -33,7 +35,70 @@ class ski_mountain():
         resort_trams = driver.find_element_by_xpath("//li[contains(@class, 'trams')]").text
         self.details["trams"] = resort_trams
 
+        """Get the terrain details with a varying number of components on the page
+        The terrains section includes count of runs, area, and types of trails. But not
+        all the sections are consistently used. The code below will grab the full list and
+        turn it inot an array. We will then talk through the array where a value is identified
+        by te prior value"""
+        terrain_text = driver.find_element_by_xpath("//ul[contains(@class, 'rt_trail diamonds')]").text
+        terrain_list = terrain_text.splitlines()
+        linenumber = 0
+
+        runs = 0
+        pct_beginner = 0
+        pct_intermediate = 0
+        pct_advanced = 0
+        pct_expert = 0
+
+        for line in terrain_list:
+            if line == 'RUNS':
+                runs = float(terrain_list[linenumber + 1])
+                self.details["runs"] = terrain_list[linenumber + 1]
+            if line == 'BEGINNER RUNS':
+                pct_beginner = p2f(terrain_list[linenumber + 1])
+                self.details["pct beginner"] = pct_beginner
+            if line == 'INTERMEDIATE RUNS':
+                pct_intermediate = p2f(terrain_list[linenumber + 1])
+                self.details["pct intermediate"] = pct_intermediate
+            if line == 'ADVANCED RUNS':
+                pct_advanced = p2f(terrain_list[linenumber + 1])
+                self.details["pct advanced runs"] = pct_advanced
+            if line == 'EXPERT RUNS':
+                pct_expert = p2f(terrain_list[linenumber + 1])
+                self.details["pct expert runs"] = pct_expert
+            linenumber += 1
+
+        # Set number for any percent
+
+        if pct_beginner > 0:
+            self.details['beginner runs'] = round(runs * pct_beginner)
+        else:
+            self.details['beginner runs'] = 0
+
+        if pct_intermediate > 0:
+            self.details['intermediate runs'] = round(runs * pct_intermediate)
+        else:
+            self.details['intermediate runs'] = 0
+
+        if pct_advanced > 0:
+            self.details['advanced runs'] = round(runs * pct_advanced)
+        else:
+            self.details['advanced runs'] = 0
+
+        if pct_expert > 0:
+            self.details['expert runs'] = round(runs * pct_expert)
+        else:
+            self.details['expert runs'] = 0
+
         driver.quit()
+
+
+def p2f(x):
+    if x == "%":
+        return 0
+    else:
+        return float(x.strip('%')) / 100
+
 
 if __name__ == "__main__":
     mountain = ski_mountain("https://www.onthesnow.com/vermont/killington-resort/ski-resort.html")
--- projects/trevor/ski_resort_analysis.py
+++ projects/trevor/ski_resort_analysis.py
@@ -2,20 +2,33 @@ from ski_mountain_details import ski_mountain
 import pandas as pd
 
 if __name__ == "__main__":
-    df = pd.read_csv('resorts.csv')
+    file_name = "resorts.csv"
+    df = pd.read_csv(file_name)
     resort_data = df.to_dict()
 
     resort_data['summit'] = {}
     resort_data['drop'] = {}
+    resort_data['beginner runs'] = {}
+    resort_data['intermediate runs'] = {}
+    resort_data['advanced runs'] = {}
+    resort_data['expert runs'] = {}
+
+
 
     for row in resort_data['name']:
         print(f"Updating values for {resort_data['name'][row]} at {resort_data['link'][row]}... ", end='')
         my_mountain = ski_mountain(resort_data['link'][row])
         resort_data['summit'][row] = my_mountain.details['summit']
         resort_data['drop'][row] = my_mountain.details['drop']
+        resort_data['beginner runs'][row] = my_mountain.details['beginner runs']
+        resort_data['intermediate runs'][row] = my_mountain.details['intermediate runs']
+        resort_data['advanced runs'][row] = my_mountain.details['advanced runs']
+        resort_data['expert runs'][row] = my_mountain.details['expert runs']
+        updated_df = pd.DataFrame(data=resort_data)
+        updated_df.to_csv(file_name)
         print(f"Success!")
 
+
     updated_df = pd.DataFrame(data=resort_data)
-    file_name = "resort_details.csv"
     updated_df.to_csv(file_name)
     print(f"Ski resort data successfully updated and written to the file {file_name}.")
