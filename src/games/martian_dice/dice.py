801e9e2ee9815344524437de9a959ec8435a0874
Brian Bozzuto <brian@brianbozzuto.com>
2021-03-23 23:59:27 -0400

    Added an animation for the deathrays, but something is not quite working right. About half the time, a death ray gets orphaned at the top...

--- src/games/martian_dice/dice.py
+++ src/games/martian_dice/dice.py
@@ -4,25 +4,41 @@ from time import time
 
 
 class Die:
-    def __init__(self):
+    def __init__(self, start_blank=False):
         self.faces = ['chicken', 'cow', 'human', 'tank', 'death-ray', 'death-ray']
-        self.value = self.roll()
+        self.active = True
+        if start_blank:
+            self.value = 'blank'
+        else:
+            self.value = self.roll()
 
     def roll(self):
-        seed(time())
-        return choice(self.faces)
+        if self.active:
+            seed(time())
+            self.value = choice(self.faces)
+        return self.value
+
 
 class Dice():
-    def __init__(self, quantity):
-        x = 0
+    def __init__(self, quantity, start_blank=False):
+        self.quantity = quantity
         self.dice = []
-        while x < quantity:
-            self.dice.append(Die())
+        self.new_roll = True
+        x = 0
+        while x < self.quantity:
+            self.dice.append(Die(start_blank))
             x += 1
+        self.sort()
+
+    def reroll(self):
+        self.new_roll = True
+        for each_die in self.dice:
+            each_die.roll()
         self.sort()
 
     def sort(self):
         self.dice = sorted(self.dice, key=lambda die: die.value)
+
 
 if __name__ == "__main__":
     x = 0
